00197be545c84e22ac2ed99e433429cfe37a3fa1
DAI\DAI <leliendai.t@gmail.com>
2026-08-31 02:33:21 +0700

    CLEANUP: Don dep cac file va thu muc thua khong su dung, toi uu ma nguon sieu nhe dat 0 loi

--- lib/services/file_downloader.dart
+++
@@ -1,8 +0,0 @@
-import 'file_downloader_stub.dart'
-    if (dart.library.html) 'file_downloader_web.dart';
-
-class FileDownloader {
-  static Future<void> download(List<int> bytes, String filename, {String mimeType = 'image/png'}) async {
-    await downloadFileBytes(bytes, filename, mimeType);
-  }
-}
--- lib/services/file_downloader_stub.dart
+++
@@ -1,3 +0,0 @@
-Future<void> downloadFileBytes(List<int> bytes, String filename, String mimeType) async {
-  // Native stub
-}
